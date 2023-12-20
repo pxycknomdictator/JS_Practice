6799cfa1ebace1ee12d9e3e1ea8490009adcbb10
Noman <pxycknomdictator@outlook.com>
2023-12-21 00:54:18 +0500

    Improve code

--- Promises/Callback-chain_async-await/callback_async_await.js
+++ Promises/Callback-chain_async-await/callback_async_await.js
@@ -3,7 +3,7 @@ const firstFun = async (timer,value) => {
          setTimeout(() => {
             let check = true
             if (check) {
-                console.log("Success "  + value)
+                console.log(`"Success " ${value} fetched`)
             }else{
                 console.log("Something is wrong 💔")
             }
@@ -14,10 +14,10 @@ const firstFun = async (timer,value) => {
 }
 
 const result = async ()=>{
-    const fetching1 = await firstFun(2000, "Data-1 💙");
-    const fetching2 = await firstFun(3000, "Data-2 💙");
-    const fetching3 = await firstFun(4000, "Data-3 💙");
-    const fetching4 = await firstFun(5000, "Data-4 💙");
+    const fetching1 = await firstFun(5000, "Data-1 💙");
+    const fetching2 = await firstFun(7000, "Data-2 💙");
+    const fetching3 = await firstFun(8000, "Data-3 💙");
+    const fetching4 = await firstFun(9000, "Data-4 💙");
     return [fetching1,fetching2,fetching3,fetching4];
 }
 
