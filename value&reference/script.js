3ed8b066ab511eef6f426eb5255007989bdc4f80
Noman <pxycknomdictator@outlook.com>
2024-03-05 22:08:49 +0500

    Adding the concept of value & reference in JavaScript

---
+++ value&reference/script.js
@@ -0,0 +1,64 @@
+// Difference Between Pass by value and Pass by reference
+
+// There are 2 types of data types 
+
+/*
+
+ 1. Primitive
+ 2. Objects
+
+  */
+
+// Primitive Data types referse pass by value
+
+// Example
+
+let first = 10;
+let second = first;
+
+first = 20;
+
+console.log(first, second); // Output will be 20 & 10
+
+// Now we know that primitive data types referse the copy of the value thats why after changing the value of first variable second value does not effect
+
+
+
+// Objects Data types referse pass by reference
+
+// Example
+
+let firstObject = {name: "Noman"};
+let secondObject = firstObject; // Assigning the first object reference or memory address to the second object.
+
+secondObject.name = "John Cena";
+
+console.log(firstObject.name); // Output John Cena not Noman
+
+// but if we assign a new object to the first object the referece of both object will be change
+
+firstObject = {name: "The rock"}; // Assign the new object to the first variable thats why now both objects has the different values.
+
+console.log(firstObject);
+console.log(secondObject);
+
+
+// Now we know that Objects does not copy its value they copy his memory space allowcated by the system and depend on a single address.
+
+// Another Example in functions
+
+const MyObject = {
+  age: 20
+}
+
+function doSomething(age) {
+  return age.age += 1;  // This line adding 1 in age;
+}
+
+console.log(doSomething(MyObject)); // Output will be 21
+
+
+console.log(MyObject.age); // Age is increament 20 => 21
+
+
+// In this function we pass the reference of the object and increament the value of origional value of object
