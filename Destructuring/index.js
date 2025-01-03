0328635d86ec4d24cd76fbcae26fd43e8d9f500f
Noman <pxycknomdictator@outlook.com>
2025-01-04 01:03:30 +0500

    add destructuring with arrays

--- Destructuring/index.js
+++ Destructuring/index.js
@@ -13,3 +13,16 @@ const { name: username, age, adult: is_adult } = object;
 // const name = object.name;
 // const age = object.age;
 // const adult = object.adult
+
+// one of the best thing is i can use destructuring in arrays
+
+const countries = ["Pakistan", "India", "Russia", "USA", "Bangladesh"];
+
+// best way
+const [pakistan, india, russia] = countries;
+
+// wrong way
+// const pakistan = countries[0]
+// const india = countries[1]
+// const russia = countries[2]
+// const usa = countries[3]
