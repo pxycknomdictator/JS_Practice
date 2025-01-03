8bac8fc1620637bfcebb832aaf3addf354689aca
Noman <pxycknomdictator@outlook.com>
2025-01-04 00:57:14 +0500

    add destructuring in js

---
+++ Destructuring/index.js
@@ -0,0 +1,12 @@
+// Destructuring is the best way to do the job done :)
+
+const object = {
+  name: "Noman",
+  age: 69,
+  adult: true,
+};
+
+// one of the worst way to do that
+const name = object.name;
+const age = object.age;
+const adult = object.adult;
