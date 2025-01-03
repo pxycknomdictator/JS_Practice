82d92893b804215f3393c7c0a9a4894bc6a14f6d
Noman <pxycknomdictator@outlook.com>
2025-01-04 01:31:49 +0500

    add rest operator

--- Destructuring/operators.js
+++ Destructuring/operators.js
@@ -14,3 +14,13 @@ const twin_brother_2 = {
   name: "Kevin",
   ...twin_brother_1,
 };
+
+// # Rest Operator lets suppose if you want to accept multiple arguments in one parameter you can use spread operator
+
+const rest_operator = (...arguments) => {
+  return arguments.reduce((accumulator, element) => {
+    return accumulator + element;
+  }, 0);
+};
+
+console.log(rest_operator(2, 4, 5, 3, 5, 2, 5));
