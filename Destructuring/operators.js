e2b638d41c6c9cefffdb148e7dad7866eae46ce0
Noman <pxycknomdictator@outlook.com>
2025-01-04 01:36:59 +0500

    add use of both rest and spread operator

--- Destructuring/operators.js
+++ Destructuring/operators.js
@@ -17,10 +17,17 @@ const twin_brother_2 = {
 
 // # Rest Operator lets suppose if you want to accept multiple arguments in one parameter you can use spread operator
 
-const rest_operator = (...arguments) => {
-  return arguments.reduce((accumulator, element) => {
-    return accumulator + element;
-  }, 0);
-};
+const rest_operator = (...arguments) =>
+  arguments.reduce((accumulator, element) => accumulator + element, 0);
 
 console.log(rest_operator(2, 4, 5, 3, 5, 2, 5));
+
+// # Let's use both rest and spread operator :D
+
+const price = [438, 2000, 192, 399, 2993, 2999, 277, 498, 229, 66];
+
+// capturing all numbers with rest operator
+const multiply_by_2 = (...prices) => prices.map((element) => element * 2);
+
+// spreading all numbers with spread operator
+console.log(multiply_by_2(...price));
