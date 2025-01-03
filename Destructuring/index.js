af7c6b91a93b605b527527cf337d6b9327e07837
Noman <pxycknomdictator@outlook.com>
2025-01-04 00:59:49 +0500

    add destructuring with alias

--- Destructuring/index.js
+++ Destructuring/index.js
@@ -6,7 +6,10 @@ const object = {
   adult: true,
 };
 
+// Best way Now i can use these values and also i can use alias to rename the keys
+const { name: username, age, adult: is_adult } = object;
+
 // one of the worst way to do that
-const name = object.name;
-const age = object.age;
-const adult = object.adult;
+// const name = object.name;
+// const age = object.age;
+// const adult = object.adult
